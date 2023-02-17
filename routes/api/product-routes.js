8f1a4e660f86f1cbab738ab5fb09a85c3c1b22dc
Thomas <tjb47.dev@gmail.com>
2023-02-16 20:31:22 -0500

    updated product-routes.js, router.delete to delete one product by it's id value

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -89,8 +89,18 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deleteProduct = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(200).json(deleteProduct)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
