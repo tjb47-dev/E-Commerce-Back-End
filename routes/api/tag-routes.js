91dd8028375ddedd66c7e4d24d42c35e5c953fa5
Thomas <tjb47.dev@gmail.com>
2023-02-16 20:40:01 -0500

    updated tag-routes.js to create, update, delete Tags by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,74 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const allTags = await Tag.findAll()
+    res.status(200).json(allTags)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagById = await Tag.findByPk(req.params.id, {
+      include: [{model: Product}]
+    })
+
+    if(!tagById) {
+      res.status(404).json({message: 'No tag found with this id.'})
+    }
+
+    res.status(200).json(tagById)
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tag = await Tag.create({
+      tag_name: req.body.tag_name
+    })
+    res.status(200).json(tag)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updateTag = await Tag.update({tag_name: req.body.tag_name},
+      {where: 
+        {
+          id: req.params.id
+        }
+      })
+    res.status(200).json(updateTag)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deleteTag = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(200).json(deleteTag)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
